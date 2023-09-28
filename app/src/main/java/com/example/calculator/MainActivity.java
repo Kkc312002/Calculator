9c0d4f33a6d8ef275312681da9d7306adc134bf4
Kkc312002 <kkc312002@gmail.com>
2023-09-28 11:33:52 +0530

    Bug fixes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,9 +9,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     boolean isNewOperator = false;
-    EditText edt1;
-    TextView txtResult;
+
+    boolean isDot = false;
+    TextView txtResult,txtMain;
     String initialNumber;
+
     String operator = "";
     double output = 0.0;
 
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edt1 = findViewById(R.id.editText);
+        txtMain = findViewById(R.id.txtMain);
         txtResult = findViewById(R.id.txtResult);
 
     }
@@ -28,37 +30,46 @@ public class MainActivity extends AppCompatActivity {
     public void numberEvent(View view) {
 
         if(!isNewOperator) {
+            if(txtMain.getText().toString().length() <= 18) {
 
-            String number = edt1.getText().toString();
+                String number = txtMain.getText().toString();
 
-            if (view.getId() == R.id.btnOne) {
-                number += "1";
-            } else if (view.getId() == R.id.btnTwo) {
-                number += "2";
-            } else if (view.getId() == R.id.btnThree) {
-                number += "3";
-            } else if (view.getId() == R.id.btnFour) {
-                number += "4";
-            } else if (view.getId() == R.id.btnFive) {
-                number += "5";
-            } else if (view.getId() == R.id.btnSix) {
-                number += "6";
-            } else if (view.getId() == R.id.btnSeven) {
-                number += "7";
-            } else if (view.getId() == R.id.btnEight) {
-                number += "8";
-            } else if (view.getId() == R.id.btnNine) {
-                number += "9";
-            } else if (view.getId() == R.id.btnZero) {
-                number += "0";
-            } else if (view.getId() == R.id.btnDZero) {
-                number += "00";
-            } else if (view.getId() == R.id.btnDot) {
-                number += ".";
+                if (view.getId() == R.id.btnOne) {
+                    number += "1";
+                } else if (view.getId() == R.id.btnTwo) {
+                    number += "2";
+                } else if (view.getId() == R.id.btnThree) {
+                    number += "3";
+                } else if (view.getId() == R.id.btnFour) {
+                    number += "4";
+                } else if (view.getId() == R.id.btnFive) {
+                    number += "5";
+                } else if (view.getId() == R.id.btnSix) {
+                    number += "6";
+                } else if (view.getId() == R.id.btnSeven) {
+                    number += "7";
+                } else if (view.getId() == R.id.btnEight) {
+                    number += "8";
+                } else if (view.getId() == R.id.btnNine) {
+                    number += "9";
+                } else if (view.getId() == R.id.btnZero) {
+                    number += "0";
+                } else if (view.getId() == R.id.btnDZero) {
+                    number += "00";
+                } else if (view.getId() == R.id.btnDot && isDot == false) {
+                    number += ".";
+                    isDot = true;
+                }
+                if (txtMain.getText().toString().length() == 15) {
+                    txtMain.setTextSize(30);
+                    txtResult.setTextSize(25);
+                }
+                txtMain.setText(number);
+                txtResult.setText("=" + number);
             }
-            edt1.setText(number);
         }else{
-            String newNumber = "";
+            isDot = false;
+            String newNumber="";
             if (view.getId() == R.id.btnOne) {
                 newNumber += "1";
             } else if (view.getId() == R.id.btnTwo) {
@@ -81,11 +92,12 @@ public class MainActivity extends AppCompatActivity {
                 newNumber += "0";
             } else if (view.getId() == R.id.btnDZero) {
                 newNumber += "00";
-            } else if (view.getId() == R.id.btnDot) {
+            } else if (view.getId() == R.id.btnDot && isDot == false) {
                 newNumber += ".";
+                isDot = true;
             }
-            edt1.append(newNumber);
-
+            txtMain.append(newNumber);
+            equal(view);
         }
     }
 
@@ -94,29 +106,30 @@ public class MainActivity extends AppCompatActivity {
         isNewOperator = true;
         if(output != 0.0){
             initialNumber = String.valueOf(output);
+            txtMain.setText(initialNumber);
+            txtMain.setTextSize(40);
+            txtResult.setTextSize(30);
+            txtResult.setTextColor(getResources().getColor(R.color.gray));
         }
 
-        if(view.getId() == R.id.btnMultiply){
-            operator = "*";}
-
-        else if(view.getId() == R.id.btnMinus){
-            operator = "-";}
-
-        else if(view.getId() == R.id.btnDivide){
-            operator = "/";}
-
-        else if(view.getId() == R.id.btnPlus){
+        if (view.getId() == R.id.btnMultiply) {
+            operator = "*";
+        } else if (view.getId() == R.id.btnMinus) {
+            operator = "-";
+        } else if (view.getId() == R.id.btnDivide) {
+            operator = "/";
+        } else if (view.getId() == R.id.btnPlus) {
             operator = "+";
         }
-        edt1.append(operator);
+        txtMain.setText(txtMain.getText().toString()+operator);
 
     }
 
-    public void equalEvent(View view) {
-        int index = edt1.getText().toString().lastIndexOf(operator);
+    public void equal(View view) {
+        int opIndex = txtMain.getText().toString().lastIndexOf(operator);
         if(initialNumber==null)
-            initialNumber = edt1.getText().toString().substring(0,index);
-        String newNumber = edt1.getText().toString().substring(index+1);
+            initialNumber = txtMain.getText().toString().substring(0,opIndex);
+        String newNumber = txtMain.getText().toString().substring(opIndex+1);
 
         if(operator.equals("+"))
             output = Double.parseDouble(initialNumber) + Double.parseDouble(newNumber);
@@ -134,25 +147,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void equalEvent(View view){
+        txtResult.setTextSize(40);
+        txtResult.setTextColor(this.getResources().getColor(R.color.white));
+        txtMain.setTextSize(30);
+        isNewOperator = false;
+    }
+
 
     public void clearEvent(View view) {
-        edt1.setText("");
+        txtMain.setText("");
         txtResult.setText("");
         isNewOperator = false;
+        operator = "";
         output = 0.0;
+        initialNumber = null;
     }
 
     public void delEvent(View view){
-        int lastIndex = edt1.getText().toString().length();
-        edt1.setText(edt1.getText().toString().substring(0,lastIndex-1));
-        if(edt1.getText().toString().equals(""))
+        if(txtMain.getText().toString().length() > 1) {
+            int lastIndex = txtMain.getText().toString().length();
+            if (txtMain.getText().toString().length() <= 15) {
+                txtResult.setTextSize(30);
+                txtMain.setTextColor(this.getResources().getColor(R.color.white));
+                txtMain.setTextSize(40);
+            }
+            txtResult.setTextColor(getResources().getColor(R.color.gray));
+            txtMain.setText(txtMain.getText().toString().substring(0, lastIndex - 1));
+            txtResult.setText(txtMain.getText().toString().substring(0, lastIndex - 1));
+        }else{
             clearEvent(view);
+        }
+
     }
 
     public void percentEvent(View view) {
-        double num = Double.parseDouble(edt1.getText().toString())/100;
-        edt1.setText(num+"");
-        isNewOperator = true;
+        if(txtMain.getText().toString().length() >0) {
+            double num;
+            if (!isNewOperator) {
+                num = Double.parseDouble(txtMain.getText().toString()) / 100;
+                txtMain.setText(""+num);
+                txtResult.setText("="+num);
 
+            } else {
+                int opIndex = txtMain.getText().toString().lastIndexOf(operator);
+                num = Double.parseDouble(txtMain.getText().toString().substring(opIndex + 1)) / 100;
+                txtMain.setText(txtMain.getText().toString().substring(0, opIndex + 1) + num);
+                equal(view);
+            }
+
+        }
     }
 }
